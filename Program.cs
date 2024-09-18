250d28e6bff0ec213b52f91e5c151449c32ef830
mtoan <109912739+MITOViXu@users.noreply.github.com>
2024-09-19 00:33:33 +0700

    first

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CS01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Xin chao C#");
+      Abc abc= new Abc();
+      
+    }
+
+  }
+  class Abc
+  {
+    static void XinChao()
+    {
+      Console.WriteLine("Xin chao den voi C#");
+    }
+  }
+}
