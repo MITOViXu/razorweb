6bd71b95d5f6aaf46625530a6020184388d2c11e
mtoan <109912739+MITOViXu@users.noreply.github.com>
2024-09-24 12:20:33 +0700

    docs: complete File course

--- obj/Debug/net8.0/razorweb.AssemblyInfo.cs
+++ obj/Debug/net8.0/razorweb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("razorweb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa94969b9ad3daa75711504e28d58ad7ed4acdc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b96cb656c6220203c0ff3958ad649ceefe7b194")]
 [assembly: System.Reflection.AssemblyProductAttribute("razorweb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("razorweb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
