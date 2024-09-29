11132f6328d14a26f72bffa75f01c1a7ecd5af3b
mtoan <109912739+MITOViXu@users.noreply.github.com>
2024-09-29 20:51:56 +0700

    docs: complete finding long way course

--- Program.cs
+++ Program.cs
@@ -2,78 +2,57 @@
 using static System.Console;
 using static System.ConsoleColor;
 using static System.Math;
-namespace CS29
+namespace Dp
 {
-
   class Program
   {
-    public class TestAsync01
-    {
 
-      public static void WriteLine(string s, ConsoleColor color)
-      {
-        Console.ForegroundColor = color;
-        Console.WriteLine(s);
-      }
-
-      public static Task<string> Async1(string thamso1, string thamso2)
-      {
-        Func<object, string> myfunc = (object thamso) =>
-        {
-          dynamic ts = thamso;
-          for (int i = 1; i <= 10; i++)
-          {
-            WriteLine($"{i,5} {Thread.CurrentThread.ManagedThreadId,3} Tham số {ts.x} {ts.y}",
-                ConsoleColor.Green);
-            Thread.Sleep(500);
-          }
-          return $"Kết thúc Async1! {ts.x}";
-        };
-
-        Task<string> task = new Task<string>(myfunc, new { x = thamso1, y = thamso2 });
-        task.Start();
-
-        Console.WriteLine("Async1: Làm gì đó sau khi task chạy");
-
-
-        return task;
-      }
-
-      public static Task Async2()
-      {
-
-        Action myaction = () =>
-        {
-          for (int i = 1; i <= 10; i++)
-          {
-            WriteLine($"{i,5} {Thread.CurrentThread.ManagedThreadId,3}",
-                ConsoleColor.Yellow);
-            Thread.Sleep(2000);
-          }
-        };
-        Task task = new Task(myaction);
-        task.Start();
-
-        Console.WriteLine("Async2: Làm gì đó sau khi task chạy");
-
-        return task;
-      }
-
-    }
-    // asynchronous (multi thread)
     static void Main(string[] args)
     {
-      Console.WriteLine($"{' ',5} {Thread.CurrentThread.ManagedThreadId,3} MainThread");
-      Task<string> t1 = TestAsync01.Async1("A", "B");
-      Task t2 = TestAsync01.Async2();
+      int maxn = 102;
+      int n, m;
+      int[,] a = new int[maxn, maxn];
+      int[,] f = new int[maxn, maxn];
+      n = int.Parse(ReadLine());
+      m = int.Parse(ReadLine());
 
-      Console.WriteLine("Làm gì đó ở thread chính sau khi 2 task chạy");
+      // Input matrix a[n,m]
+      for (int i = 1; i <= n; i++)
+      {
+        for (int j = 1; j <= m; j++)
+        {
+          a[i, j] = int.Parse(ReadLine());
+        }
+      }
 
-      t1.Wait();
-      String s = t1.Result;
-      TestAsync01.WriteLine(s, ConsoleColor.Red);
+      // Initialize boundary values for f
+      for (int i = 0; i <= m; i++)
+      {
+        f[0, i] = f[n + 1, i] = -100000000;
+      }
 
-      Console.ReadKey();
+      // Initialize the first column of f with the values of a, as no previous values exist
+      for (int i = 1; i <= n; i++)
+      {
+        f[i, 1] = a[i, 1];
+      }
+
+      // Dynamic programming to compute maximum path sum
+      for (int j = 2; j <= m; j++)
+      {
+        for (int i = 1; i <= n; i++)
+        {
+          f[i, j] = Max(f[i - 1, j - 1], Max(f[i, j - 1], f[i + 1, j - 1])) + a[i, j];
+        }
+      }
+
+      // Find the maximum value in the last column (f[i,m])
+      int ans = -100000000;
+      for (int i = 1; i <= n; i++)
+      {
+        ans = Max(ans, f[i, m]);
+      }
+      WriteLine("\n Final result: "+ans);
     }
   }
 }
