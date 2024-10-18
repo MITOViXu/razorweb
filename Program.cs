f708ed2c336e10e2378f82d09f9acb896ee74b16
mtoan <109912739+MITOViXu@users.noreply.github.com>
2024-10-18 18:31:24 +0700

    docs: complete Mailservice coourse

--- Program.cs
+++ Program.cs
@@ -1,39 +1,23 @@
-using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
-var servive = builder.Services;
-
-servive.AddDistributedMemoryCache();
-servive.AddDistributedSqlServerCache((option) =>
-{
-  option.ConnectionString = "Data Source=192.168.1.110, 1433; Initial Catalog=webdb; TrustServerCertificate=True; UID = SA; PWD = Password123";
-  option.SchemaName = "dbo";
-  option.TableName = "Session";
-});
-servive.AddSession((option) =>
-{
-  option.Cookie.Name = "mtoan";
-  option.IdleTimeout = new TimeSpan(0, 30, 0);
-  //                            hour, minute, seconds
-});
 var app = builder.Build();
-app.UseSession(); // Session middleware
-app.MapGet("/readwritesession", async (context) =>
-{
-  int? count;
-  count = context.Session.GetInt32("count");
-  if (count == null) count = 0;
-  count += 1;
-  context.Session.SetInt32("count", count.Value);
-  await context.Response.WriteAsync("So lan truy cap : " + count);
-});
-app.MapGet("/", async (context) =>
-{
-  context.Response.ContentType = "text/plain; charset=utf-8";
-  await context.Response.WriteAsync("Trang web trống");
-});
 
-app.Run();
 /*
-dotnet sql-cache create "Data Source=192.168.1.110, 1433; Initial Catalog=webdb; TrustServerCertificate=True; UID = SA; PWD = Password123" dbo Session;
+
+If we want to send Mail, we must to have a server host 
+Mail Server - smtp
+Smtp
+
 */
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/Test", async (context) =>
+{
+  var mess = await MailUtils.SendMail("mtoansecond@gmail.com", "mtoansecond@gmail.com", "TEST", "Xin chao mtoan");
+  await context.Response.WriteAsync(mess);
+});
+app.MapGet("/TestGmail", async (context) =>
+{
+  var mess = await MailUtils.SendGMail("mtoan@gmail.com", "minhtoan250503@gmail.com", "TEST", "Xin chao mtoan", "mtoan@gmail.com", "hello");
+  await context.Response.WriteAsync(mess);
+});
+app.Run();
